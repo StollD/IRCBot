1df4a9826cab7b9310510e20dc17f614246bf7c1
Thomas <dorianstoll@googlemail.com>
2015-10-02 23:50:49 +0200

    Use try-catch for tell

--- IRCBot/Commands/Tell.cs
+++ IRCBot/Commands/Tell.cs
@@ -14,26 +14,34 @@ namespace IRCBot
             // Get the attributes
             string args = Utils.Remove(msg, "tell", true);
 
-            // Get the username etc
-            int i = 0;
-            for (i = 0; i < args.Length; i++)
-                if (args[i] == ' ')
-                    break;
-            string name = args.Remove(i);
-            args = args.Substring(i).Trim();
+            // Man...
+            try
+            {
+                // Get the username etc
+                int i = 0;
+                for (i = 0; i < args.Length; i++)
+                    if (args[i] == ' ')
+                        break;
+                string name = args.Remove(i);
+                args = args.Substring(i).Trim();
 
-            // Private
-            bool pn = Is(args, "private");
+                // Private
+                bool pn = Is(args, "private");
 
-            // Message
-            string mess = Utils.Remove(args, "private");
+                // Message
+                string mess = Utils.Remove(args, "private");
 
-            // Check if the list exists
-            if (!seenTell.tell.ContainsKey(name))
-                seenTell.tell.Add(name, new List<List<object>>());
-            seenTell.tell[name].Add(new List<object>() { channel.Name, message.User.Nick, DateTime.Now, mess, pn });
-            SendMessage(message.User.Nick + ": I'll redirect this as soon as he/she/it is around.");
-            Utils.Save(seenTell);
+                // Check if the list exists
+                if (!seenTell.tell.ContainsKey(name))
+                    seenTell.tell.Add(name, new List<List<object>>());
+                seenTell.tell[name].Add(new List<object>() { channel.Name, message.User.Nick, DateTime.Now, mess, pn });
+                SendMessage(message.User.Nick + ": I'll redirect this as soon as he/she/it is around.");
+                Utils.Save(seenTell);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
